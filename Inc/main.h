2e1529954b5d14b1239bf18004c2be64ad701b59
WillCoulson <1273646327@qq.com>
2020-07-14 17:00:09 +0800

    Third: SysLED undercontrol.

--- Inc/main.h
+++ Inc/main.h
@@ -58,6 +58,8 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define Sys_LED_H_Pin GPIO_PIN_0
+#define Sys_LED_H_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
